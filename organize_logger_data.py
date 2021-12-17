71951c6c1f3f7ba31711985b1660feda0aae2c90
ryantheengineer <ryan.larson.wa@gmail.com>
2021-12-17 13:41:12 -0700

    Commit before branching to try simplifying required columns

--- organize_logger_data.py
+++ organize_logger_data.py
@@ -17,13 +17,14 @@ import glob
 from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWUSR
 from csv import writer
 
+
 # Set the directory where the data logs will be kept
 directory = r"Z:\Safety\Inspections & Assessments\Air Samplings\PAC 8000 Data Logs"
 namepattern = "ARPL-1307_PAC 8000*.txt"
 filepattern = directory + "\\" + namepattern
 
 ### Check for new data files against a logged list ###
-logfile = directory + "\\LOGGED.csv"
+logfile = directory + "\\LOGGED.csv"    # NOTE: LATER THIS SHOULD VERIFY THAT THE EXPECTED FILE STRUCTURE EXISTS BEFORE PROCEEDING
 os.chmod(logfile, S_IWUSR|S_IREAD)  # This makes the log file read/write
 
 # Read the logfile data into dataframe
@@ -57,7 +58,7 @@ for file in glob.glob(filepattern):
             rowstr = str(rawdata.loc[i])
             rowlist = rowstr.split(";")
             
-            if any("VAL" in string for string in rowlist):
+            if any("VAL" in string for string in rowlist):      # Need to add triggers for other event types of interest
                 eventtype.append("VAL")
                 dtime  = dt.datetime.strptime(rowlist[1],"%Y%m%d%H%M%S")        
                 datetime.append(dtime)
@@ -79,20 +80,90 @@ for file in glob.glob(filepattern):
                                  "Time":time,
                                  "Styrene":styrene})
         
-        csvname = os.path.splitext(base)[0] + ".csv"
-        csvfile = directory + "\\" + csvname
+        pklname = os.path.splitext(base)[0] + ".pkl"
+        pklfile = directory + "\\" + pklname
         
-        measdata.to_csv(csvfile, mode='a')  # Write measdata to csv
+        measdata.to_pickle(pklfile)
         
-        os.chmod(csvfile, S_IREAD|S_IRGRP|S_IROTH)  # Set the csvfile as read-only
+        # csvname = os.path.splitext(base)[0] + ".csv"
+        # csvfile = directory + "\\" + csvname
         
-        # Append the datalog file name LOGGED.csv
-        with open(logfile, 'a', newline='') as f_object:
-            writer_object = writer(f_object)    # Pass the CSV file object to the writer() function
-            writer_object.writerow([base])      # Pass the datalog filename as an argument into the writerow() function
-            f_object.close()                    # Close the file object
+        # measdata.to_csv(csvfile, mode='a', index=False)  # Write measdata to csv
+        
+        # os.chmod(csvfile, S_IREAD|S_IRGRP|S_IROTH)  # Set the csvfile as read-only
+        
+        # # Append the datalog file name LOGGED.csv
+        # with open(logfile, 'a', newline='') as f_object:
+        #     writer_object = writer(f_object)    # Pass the CSV file object to the writer() function
+        #     writer_object.writerow([base])      # Pass the datalog filename as an argument into the writerow() function
+        #     f_object.close()                    # Close the file object
 
 
 print("All remaining files have been logged.")
 
 os.chmod(logfile, S_IREAD|S_IRGRP|S_IROTH)  # Make logfile read-only after writing new datalogger filenames to list
+
+
+### Break down the CSV files by date for easy searching ###
+processedfile = directory + "\\By Day\\PROCESSED.csv"
+os.chmod(processedfile, S_IWUSR|S_IREAD)  # This makes the log file read/write
+
+# Read the processedfile data into dataframe
+try:
+    processed = pd.read_csv(processedfile)
+except pd.errors.EmptyDataError:
+    print("PROCESSED.csv is empty\n")
+    
+pnamepattern = "ARPL-1307_PAC 8000*.csv"
+pfilepattern = directory + "\\" + pnamepattern
+
+for file in glob.glob(pfilepattern):
+    base = os.path.basename(file)
+    print(base)
+    
+    # If the CSV file isn't in the list, process it
+    if not processed["ProcessedData"].str.contains(base).any():
+        print("{} is not in the processed list".format(base))
+        
+        measdata = pd.read_csv(file, parse_dates=True)    # Read the CSV data in to a DataFrame
+        measdata["Date"] = pd.to_datetime(measdata["Date"])
+        measdata["Date"] = measdata["Date"].dt.date
+        
+        measdata["Time"] = pd.to_datetime(measdata["Time"])
+        measdata["Time"] = measdata["Time"].dt.time
+        
+        measdata["DateTime"] = pd.to_datetime(measdata["DateTime"])
+        
+        # # daytimestr = 
+        # daytimes = []
+        for ind in measdata.index:
+            measdata.loc[ind,"DateTime"] = measdata.loc[ind,"DateTime"].to_pydatetime()
+        
+        # Get list of unique dates in measdata
+        uniquedates = pd.to_datetime(measdata["DateTime"]).dt.date.unique()
+            
+        
+        # For each unique date, create a DataFrame with data only from that date
+        for day in uniquedates:
+            print(type(day))
+            print(type(measdata["DateTime"][1]))
+            dateindices = measdata["Date"] == day
+            datedata = measdata.loc[dateindices]
+            
+            # Generate the CSV name based on date
+            daystr = day.strftime("%Y-%m-%d")
+            csvname = daystr + "_Styrene.csv"
+            csvfile = directory + "\\By Day\\" + csvname
+            
+            # Check if a CSV file exists in the By Day folder with the same name
+            dayfilepattern = directory + "\\By Day\\*_Styrene.csv"
+            for dayfile in glob.glob(dayfilepattern):
+                print(dayfile)
+                
+                if csvname == dayfile:
+                    # If the generated CSV name has the same name as an existing CSV file,
+                    # open the existing file and append any new values from the current
+                    # CSV file, if any exist.
+                    print("\nA file already exists with that name")
+                else:
+                    print("File {} is not in the processed list".format(dayfile))
