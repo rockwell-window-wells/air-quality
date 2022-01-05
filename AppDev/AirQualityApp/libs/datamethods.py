18650de4b9eccad719434622fa63c090af0037b5
ryantheengineer <ryan.larson.wa@gmail.com>
2022-01-05 12:14:03 -0700

    Fixed remaining file structure issues

--- AppDev/AirQualityApp/libs/datamethods.py
+++ AppDev/AirQualityApp/libs/datamethods.py
@@ -32,11 +32,12 @@ def refresh_data(data_directory):
         print(logfile)
         if exists(logfile) is False:
             print("No LOGGED.csv file exists in directory. Creating one now.")
-            with open(logfile, 'w', encoding='UTF8') as f:
+            with open(logfile, 'w', encoding='UTF8', newline='') as f:
                 # create the csv writer
                 csvwriter = writer(f)
                 # Write the header row for the LOGGED file
                 csvwriter.writerow(["DataLog"])
+                f.close()
 
         os.chmod(logfile, S_IWUSR|S_IREAD)  # This makes the log file read/write
 
@@ -118,11 +119,13 @@ def refresh_data(data_directory):
         # If there is no PROCESSED file in the directory, then create one
         if exists(processedfile) is False:
             print("No PROCESSED.csv file exists in directory. Creating one now.")
-            with open(logfile, 'w', encoding='UTF8') as f:
+            with open(processedfile, 'w', encoding='UTF8', newline='') as f:
                 # create the csv writer
                 csvwriter = writer(f)
                 # Write the header row for the LOGGED file
                 csvwriter.writerow(["ProcessedData"])
+                f.close()
+
         os.chmod(processedfile, S_IWUSR|S_IREAD)  # This makes the processed file read/write
 
         # Read the processedfile data into dataframe
@@ -134,79 +137,81 @@ def refresh_data(data_directory):
         pnamepattern = "*PAC 8000*.pkl"
         pfilepattern = directory + "\\" + pnamepattern
 
-        if not glob.glob(pfilepattern):
-            print("ERROR: No processed data .pkl files exist in the chosen directory.")
-            message = "ERROR: No processed data .pkl files exist in the chosen directory."
-            break
-            # raise Exception("No processed data .pkl files exist in the chosen data directory.")
-        else:
-            for file in glob.glob(pfilepattern):
-                base = os.path.basename(file)
-                print("Checking for {}....".format(base))
+        # Check for the By Day folder
+        bydaydirectory = directory + "\\By Day"
+        if not os.path.isdir(bydaydirectory):
+            print("By Day folder doesn't exist. Making it now.")
+            os.mkdir(bydaydirectory)
 
-                # If the PKL file isn't in the list, process it
-                if not processed["ProcessedData"].str.contains(base).any():
-                    print("{} is not in the processed list".format(base))
+        for file in glob.glob(pfilepattern):
+            base = os.path.basename(file)
+            print("Checking for {}....".format(base))
 
-                    measdata = pd.read_pickle(file)    # Read the PKL data in to a DataFrame
+            # If the PKL file isn't in the list, process it
+            if not processed["ProcessedData"].str.contains(base).any():
+                print("{} is not in the processed list".format(base))
 
-                    uniquedates = pd.to_datetime(measdata["DateTime"]).dt.date.unique()
-                    # print(uniquedates)
+                measdata = pd.read_pickle(file)    # Read the PKL data in to a DataFrame
 
-                    # For each unique date, create a DataFrame with data only from that date
-                    for day in uniquedates:
-                        midnight = dt.time(0,0,0)
-                        start_datetime = dt.datetime.combine(day,midnight)
-                        endday = day + dt.timedelta(days=1)
-                        end_datetime = dt.datetime.combine(endday,midnight)
+                uniquedates = pd.to_datetime(measdata["DateTime"]).dt.date.unique()
+                # print(uniquedates)
 
-                        after_start = measdata["DateTime"] >= start_datetime
-                        before_end = measdata["DateTime"] < end_datetime
-                        between_times = after_start & before_end
-                        on_day = measdata.loc[between_times]    # DataFrame of only the current day's data
+                # For each unique date, create a DataFrame with data only from that date
+                for day in uniquedates:
+                    midnight = dt.time(0,0,0)
+                    start_datetime = dt.datetime.combine(day,midnight)
+                    endday = day + dt.timedelta(days=1)
+                    end_datetime = dt.datetime.combine(endday,midnight)
 
-                        # Generate the PKL name based on date
-                        pname = str(day) + "_Styrene.pkl"
-                        pfile = directory + "\\By Day\\" + pname        # Is this redundant?
+                    after_start = measdata["DateTime"] >= start_datetime
+                    before_end = measdata["DateTime"] < end_datetime
+                    between_times = after_start & before_end
+                    on_day = measdata.loc[between_times]    # DataFrame of only the current day's data
 
-                        pklname = str(day) + "_Styrene.pkl"
-                        pklfile = directory + "\\By Day\\" + pklname
+                    # Generate the PKL name based on date
+                    pname = str(day) + "_Styrene.pkl"
+                    pfile = directory + "\\By Day\\" + pname        # Is this redundant?
 
-                        # Look in By Day folder for duplicate files of pklfile. If a file already
-                        # exists, load its data and concatenate the new data with it. Then
-                        # remove any duplicates and save the complete file back to the same
-                        # name. This way each unique date we have data for should have a single
-                        # file assigned to it.
-                        if os.path.exists(pklfile):
-                            print("Existing data found for {}".format(day))
-                            existingdata = pd.read_pickle(pklfile)
+                    pklname = str(day) + "_Styrene.pkl"
+                    pklfile = directory + "\\By Day\\" + pklname
 
-                            # Concatenate the existing data and new data
-                            # Use pd.concat([data1, data2], axis=0) and then df = df.drop_duplicates(subset="DateTime", keep="first")
-                            complete_day = pd.concat([existingdata, on_day], axis=0, ignore_index=True)
-                            complete_day = complete_day.drop_duplicates(subset="DateTime", keep="first")
-                            complete_day = complete_day.reset_index(drop=True)
-                            print("Data combined for {}".format(day))
+                    # Look in By Day folder for duplicate files of pklfile. If a file already
+                    # exists, load its data and concatenate the new data with it. Then
+                    # remove any duplicates and save the complete file back to the same
+                    # name. This way each unique date we have data for should have a single
+                    # file assigned to it.
+                    if os.path.exists(pklfile):
+                        print("Existing data found for {}".format(day))
+                        existingdata = pd.read_pickle(pklfile)
 
-                            # Save complete_day as a PKL file
-                            complete_day.to_pickle(pklfile)
+                        # Concatenate the existing data and new data
+                        # Use pd.concat([data1, data2], axis=0) and then df = df.drop_duplicates(subset="DateTime", keep="first")
+                        complete_day = pd.concat([existingdata, on_day], axis=0, ignore_index=True)
+                        complete_day = complete_day.drop_duplicates(subset="DateTime", keep="first")
+                        complete_day = complete_day.reset_index(drop=True)
+                        print("Data combined for {}".format(day))
 
-                        else:
-                            print("No existing data found for {}".format(day))
-                            on_day = on_day.reset_index(drop=True)
-                            on_day.to_pickle(pklfile) # Create the PKL file for the current data log file
+                        # Save complete_day as a PKL file
+                        complete_day.to_pickle(pklfile)
+
+                    else:
+                        print("No existing data found for {}".format(day))
+                        on_day = on_day.reset_index(drop=True)
+                        on_day.to_pickle(pklfile) # Create the PKL file for the current data log file
 
 
-                    # Append the single day PKL file name to PROCESSED.csv
-                    # Append the datalog file name LOGGED.csv
-                    with open(processedfile, 'a', newline='') as f_object:
-                        writer_object = writer(f_object)    # Pass the CSV file object to the writer() function
-                        writer_object.writerow([base])      # Pass the datalog filename as an argument into the writerow() function
-                        f_object.close()                    # Close the file object
+                # Append the single day PKL file name to PROCESSED.csv
+                # Append the datalog file name LOGGED.csv
+                with open(processedfile, 'a', newline='') as f_object:
+                    writer_object = writer(f_object)    # Pass the CSV file object to the writer() function
+                    writer_object.writerow([base])      # Pass the datalog filename as an argument into the writerow() function
+                    f_object.close()                    # Close the file object
 
         print("All processed data .pkl files have been broken down by day.")
 
         os.chmod(processedfile, S_IREAD|S_IRGRP|S_IROTH)  # Make processedfile read-only after writing new processed filenames to list
+
+        break
 
 
     return message
